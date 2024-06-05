6df2c6757a7488a06056b138839317e8509289a7
James <jamesolmeda17@gmail.com>
2024-06-05 19:44:21 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+#!/bin/bash
 
 
+# Create a new virtual environment
+python -m venv antenv
+
+# Activate the virtual environment
+source atenv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+
+# Set up necessary director
 # Start Odoo
 exec odoo-bin -c odoo.conf
